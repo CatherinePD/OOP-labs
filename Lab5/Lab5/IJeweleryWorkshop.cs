3fed410f8504748af2a124b3b984dd8f9f193276
Kate <katekw126p@gmail.com>
2018-10-23 22:08:03 +0300

    Lab 5

---
+++ Lab5/Lab5/IJeweleryWorkshop.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab5
+{
+    public interface IJeweleryWorkshop  // ювелирная мастерская
+    {
+        void MakeRing();  // сделать кольцо
+        void MakeEarrings(); // сделать серьги
+        void ProcessStone(); // обработать камень для создания ювелирного изделия
+    }
+}
